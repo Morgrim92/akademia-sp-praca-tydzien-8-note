c04c2751368489c56488318258c5018744535317
przemyslaw.luczak <przemyslaw.luczak.92@gmail.com>
2020-11-28 14:15:20 +0100

    initial commit

---
+++ src/main/java/pl/luczak/przemyslaw/pracadomowatydzien8note/PracaDomowaTydzien8NoteApplication.java
@@ -0,0 +1,13 @@
+package pl.luczak.przemyslaw.pracadomowatydzien8note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaDomowaTydzien8NoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaDomowaTydzien8NoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/luczak/przemyslaw/pracadomowatydzien8note/PracaDomowaTydzien8NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.luczak.przemyslaw.pracadomowatydzien8note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaDomowaTydzien8NoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
